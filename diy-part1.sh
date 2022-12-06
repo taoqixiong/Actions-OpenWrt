27beed1bfeeb360f2fa73c943a8e8329cd145959
Azunyan <taoqixiong1418@gmail.com>
2022-12-06 17:34:50 +0800

    .

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+#sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
-git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone --depth 1 -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone --depth 1 -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+#git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+#git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+
+git clone --depth 1 https://github.com/fw876/helloworld.git package/ssr
+git clone --depth 1 https://github.com/firker/diy-ziyong.git package/diy-ziyong
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone --depth 1 https://github.com/linkease/istore.git package/istore
+git clone --depth 1 https://github.com/linkease/istore-ui.git package/istore-ui
+git clone --depth 1 https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/wrtbwmon
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt VIP99 '/g" ./package/lean/default-settings/files/zzz-default-settings
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files/bin/config_generate
+#sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+#sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt VIP99 '/g" ./package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files/bin/config_generate
 
-rm -rf ./package/lean/luci-theme-argon
-rm -rf ./feeds/passwall/xray-plugin
-rm -rf ./feeds/packages/net/https-dns-proxy
+#rm -rf ./package/lean/luci-theme-argon
+#rm -rf ./feeds/passwall/xray-plugin
+#rm -rf ./feeds/packages/net/https-dns-proxy
 
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+#svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+
+curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/luci/master/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update > ./feeds/luci/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update
+curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
+
+sed -i 's/bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
+rm -rf ./package/diy-ziyong/theme
+rm -rf ./package/diy-ziyong/luci-app-wrtbwmon
+rm -rf ./package/diy-ziyong/wrtbwmon
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/mosdns
