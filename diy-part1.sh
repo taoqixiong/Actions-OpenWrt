a1efefadf904b1f7141ee0aebd60186573a60759
Azunyan <taoqixiong1418@gmail.com>
2022-12-06 17:12:39 +0800

    .

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
-
-sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
